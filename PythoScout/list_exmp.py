e34b98ce7749f5cab75834dd3c92cd5bf8f0d4fc
Tanvir Anowar <tanvir.net@gmail.com>
2025-11-10 00:09:22 +0000

    Refactor list_exmp.py to comment out unused code and improve readability

--- PythoScout/list_exmp.py
+++ PythoScout/list_exmp.py
@@ -1,4 +1,13 @@
 name = ["Alice", "Bob", "Charlie"]
 
-print(f"List of names:{name[2]}")
+# print(f"List of names:{name[2]}")
+
+#for n in range(len(name)):
+ #   print(f"*{name[n]}")
  
+
+
+number_collection = [1, 2, 3, 4, 5,6,7,8,9,10]
+
+for num in range(1,5):
+    print(number_collection[num])
