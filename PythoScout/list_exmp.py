c864699b2e0d2d36567f940d69011ce97c8888ef
Tanvir Anowar <tanvir.net@gmail.com>
2025-11-10 23:21:54 +0000

    Update list_exmp.py to add example dictionary and enhance code structure

--- PythoScout/list_exmp.py
+++ PythoScout/list_exmp.py
@@ -1,3 +1,4 @@
+'''
 name = ["Alice", "Bob", "Charlie"]
 
 # print(f"List of names:{name[2]}")
@@ -11,3 +12,17 @@ number_collection = [1, 2, 3, 4, 5,6,7,8,9,10]
 
 for num in range(1,5):
     print(number_collection[num])
+    
+   '''
+    
+dic_exmp = {
+    "name": "Alice",
+    "age": 30,
+    "city": "New York"
+}
+
+for key,value in dic_exmp.items(): # need to convert to items() to get both key and value
+    print(f"{key}:{value}")
+    
+
+    
