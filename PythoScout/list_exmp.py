ffdc94162de27cf5ba847a856fb12202a7662a2b
Tanvir Anowar <tanvir.net@gmail.com>
2025-11-11 00:00:49 +0000

    Refactor list_exmp.py to improve code structure and readability by organizing dictionary examples and enhancing set operations

--- PythoScout/list_exmp.py
+++ PythoScout/list_exmp.py
@@ -13,7 +13,7 @@ number_collection = [1, 2, 3, 4, 5,6,7,8,9,10]
 for num in range(1,5):
     print(number_collection[num])
     
-   '''
+
     
 dic_exmp = {
     "name": "Alice",
@@ -21,8 +21,38 @@ dic_exmp = {
     "city": "New York"
 }
 
-for key,value in dic_exmp.items(): # need to convert to items() to get both key and value
-    print(f"{key}:{value}")
+dic_exmp_2 = {
+    "product": "Laptop",
+    "price": 1200,
+    "brand": "Dell"
+}
+
+
+
+envronment_values =[dic_exmp, dic_exmp_2]
+
+for env in envronment_values:
+    print(env.values())
     
 
+for key,value in dic_exmp.items(): # need to convert to items() to get both key and value
+    print(f"{key}:{value}")
+   # print(f"{}")
     
+ 
+# Example Set
+example_set = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1 }
+
+check_set = {3, 4, 5, 99,88}
+
+print(example_set.intersection(check_set))
+
+print(example_set.union(check_set))
+'''  
+
+#removes duplicates by converting to set and back to list
+list_data = list( set([ "apple", "banana", "cherry", "date", "elderberry" ,"banana"])) 
+
+print(list_data)
+
+
